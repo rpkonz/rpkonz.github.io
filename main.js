6616f45f6f8b1cde97ed685354618271ddd15bd7
Ryan Konz <rpkonz@users.noreply.github.com>
2018-03-09 19:34:22 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+<script>
+$.ajax({
+  url: "https://api.magicthegathering.io/v1/cards?name=nissa's chosen"
+}).done(function(data) {
+  console.log(data);
+  $("body").append(data.cards[0].name);
+  $("body").append(" -- ");
+  $("body").append(data.cards[0].manaCost);
+  $("body").append(data.cards[0].type);
+  $("body").append(data.cards[0].text);
+  $("body").append(data.cards[0].power);
+	$("body").append(" / ");  
+  $("body").append(data.cards[0].toughness);
+	$("body").append("<br/>");  
+});
+</script>
