a799d6d871f72d83ac62492958d25f50c9aaf6ac
Ryan Konz <rpkonz@users.noreply.github.com>
2018-03-09 19:46:52 -0500

    Update main.js

--- main.js
+++ main.js
@@ -3,14 +3,14 @@ $(document).ready(function(){
 	  url: "https://api.magicthegathering.io/v1/cards?name=nissa's chosen"
 	}).done(function(data) {
 	  console.log(data);
-	  $("body").append(data.cards[0].name);
-	  $("body").append(" -- ");
-	  $("body").append(data.cards[0].manaCost);
-	  $("body").append(data.cards[0].type);
-	  $("body").append(data.cards[0].text);
-	  $("body").append(data.cards[0].power);
-		$("body").append(" / ");  
-	  $("body").append(data.cards[0].toughness);
-		$("body").append("<br/>");  
+	  $("#header").append(data.cards[0].name);
+	  $("#header").append(" -- ");
+	  $("#header").append(data.cards[0].manaCost);
+	  $("#type").append(data.cards[0].type);
+	  $("#text").append(data.cards[0].text);
+	  $("#power").append(data.cards[0].power);
+		$("#power").append(" / ");  
+	  $("#power").append(data.cards[0].toughness);
+		$("#power").append("<br/>");  
 	});
 });
